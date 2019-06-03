ed4a30e4a4fdb03426514398d2252a3bdb6a1784
Pushkar Kolhe <pushkar@moquality.com>
2019-06-03 18:49:55 -0400

    Shows path to WDA

--- wda.sh
+++ wda.sh
@@ -1,4 +1,5 @@
 APPIUMPATH="$(which appium)"
 APPIUMDIR="$(echo $APPIUMPATH | sed -e "s/\/bin\/appium//g" )"
 WDAPATH="$(find $APPIUMDIR -name WebDriverAgent.xcodeproj)"
+echo $WDAPATH
 open $WDAPATH
